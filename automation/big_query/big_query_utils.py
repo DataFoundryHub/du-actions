d9b720557f10ebf807387e358f72c3921a49295c
AADARSH GAIKWAD <aadarshgaikwad96@gmail.com>
2024-04-23 00:38:57 -0400

    updated the code

--- automation/big_query/big_query_utils.py
+++ automation/big_query/big_query_utils.py
@@ -16,7 +16,7 @@ def write_to_bigquery(data):
     rows_to_insert = [(data['value1'], data['value2'])]  # Customize based on your data
     errors = client.insert_rows(table_ref, rows_to_insert)
 
-    if errors == []:
+    if not errors:
         print('Data loaded into BigQuery table successfully.')
     else:
         print('Errors occurred while inserting rows:', errors)
